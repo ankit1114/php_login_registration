29526fe8d6f22240063d2a2b11fed0ef5dfec889
ankit1114 <46845315+ankit1114@users.noreply.github.com>
2019-09-14 10:31:02 +0530

    first commmit

---
+++ page-about.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include("header.php");
+?>
+<div class="container">
+<h1>About Me</h1>
+<p>A short paragraph on myself.</p>
+</div>
+<nav>
+ <div>
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>About Me</h2>
+      <h5>Photo of me:</h5>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <h3>Some Links</h3>
+      <p>Lorem ipsum dolor sit ame.</p>
+
+      <hr class="hidden-sm hidden-md hidden-lg">
+    </div>
+  </div>
+ </div>
+</nav>
+<?php
+include("footer.php");
+?>
