b7af3a14a79dc7cda24979d686bbcf390beb23e8
ankit1114 <46845315+ankit1114@users.noreply.github.com>
2019-09-14 10:12:57 +0530

    first commit

--- db.php
+++
@@ -1,8 +0,0 @@
- <?php
-    $sql = "CREATE TABLE `users` (\n"
-    . " `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,\n"
-    . " `username` varchar(100) NOT NULL,\n"
-    . " `email` varchar(100) NOT NULL,\n"
-    . " `password` varchar(100) NOT NULL\n"
-    . ") ENGINE=InnoDB DEFAULT CHARSET=latin1";
-    ?>
