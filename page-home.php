07181180f27210863d8000c34dd4e7db5db8107e
ankit1114 <46845315+ankit1114@users.noreply.github.com>
2019-09-14 10:28:20 +0530

    first commit

---
+++ page-home.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include("header.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<h1>Welcome Home</h1>
+
+</body>
+</html>
+<?php
+include("footer.php");
+?>
